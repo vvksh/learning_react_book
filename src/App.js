132f578a392e96ed78ed91ea85545b90378983dc
Vivek Sah <vivek.sah32+github@gmail.com>
2020-12-27 22:26:30 -0800

    chapter 5- recipe renders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+
+const data = [
+  {
+    name: "Baked Salmon",
+    ingredients: [
+      { name: "Salmon", amount: 1, measurement: "l lb" },
+      { name: "Pine Nuts", amount: 1, measurement: "cup" },
+      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
+      { name: "Yellow Squash", amount: 1, measurement: "med" },
+      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
+      { name: "Garlic", amount: 3, measurement: "cloves" }
+    ],
+    steps: [
+      "Preheat the oven to 350 degrees.",
+      "Spread the olive oil around a glass baking dish.",
+      "Add the yellow squash and place in the oven for 30 mins.",
+      "Add the salmon, garlic, and pine nuts to the dish.",
+      "Bake for 15 minutes.",
+      "Remove from oven. Add the lettuce and serve."
+    ]
+  },
+  {
+    name: "Fish Tacos",
+    ingredients: [
+      { name: "Whitefish", amount: 1, measurement: "l lb" },
+      { name: "Cheese", amount: 1, measurement: "cup" },
+      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
+      { name: "Tomatoes", amount: 2, measurement: "large" },
+      { name: "Tortillas", amount: 3, measurement: "med" }
+    ],
+    steps: [
+      "Cook the fish on the grill until cooked through.",
+      "Place the fish on the 3 tortillas.",
+      "Top them with lettuce, tomatoes, and cheese."
+    ]
+  }
+];
+
+
+// A function component for an individual Recipe
+function Recipe ( {name, ingredients, steps}) {
+  return (
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
+      <h1>{name}</h1>
+      <ul className="ingredients">
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient.name}</li>
+        ))}
+      </ul>
+      <section className="instructions">
+        <h2>Cooking instructions</h2>
+        {steps.map((step, i) => (
+          <p key={i}>{step}</p>
+        ))}
+      </section>
+    </section>
+  )
+}
+
+// A function component for the Menu of Recipes
+function Menu (props) { // can also deconstruct props by keys like Menu ( { title, recipes}) 
+  return (
+    <article>
+      <header>
+        <h1>{props.title}</h1>
+      </header>
+      <div className="recipes">
+        {props.recipes.map((recipe, i) => (
+          <Recipe
+            key={i}
+            name={recipe.name}
+            ingredients={recipe.ingredients}
+            steps={recipe.steps}
+          />
+        ))}
+      </div>
+    </article>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu recipes={data} title="Delicious Recipes"/>
     </div>
   );
 }
